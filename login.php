23cee2d21f5ecdc10283e41f4e4162a01aa78d99
mirwais1234 <mirwiasaraj@gmail.com>
2020-03-30 13:32:10 +0200

    new push

--- login.php
+++ login.php
@@ -1,26 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <body>
     <?php
-
     include_once("credentials.php");
     $connection = mysqli_connect($servername, $username, $password, $database);
-
     if (!$connection) {
         die("Connection failed: " . mysqli_connect_error());
     }
-
     if ((isset($_GET["Username"]))&&
      (isset($_GET["Password"]))) {
-
         $isUserFromMyDatabase = $connection->prepare("SELECT * FROM people WHERE Username=?");
         $isUserFromMyDatabase->bind_param("s", $_GET["Username"]);
         $isUserFromMyDatabase->execute();
-
         if ($result->num_rows === 1) {
             print "we are checking your password <br>";
-            $row = $result->num_rows->fetch_assoc());
+            $row = $result->num_rows->fetch_assoc();
             print $row["person_ID"];
             if ($row['Password'] === $_GET["Password"]) {
                 print "you are now successfully login!<br>";
@@ -38,8 +32,6 @@
         password: <input type="text" name="Password">
         <input type="submit" value="login" name="login">
     </form>
-
-
 </body>
 
 </html>
