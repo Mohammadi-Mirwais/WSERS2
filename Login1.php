3ab9597308eaeb66a148ef798dd4df8b1fef7588
mirwais1234 <mirwiasaraj@gmail.com>
2020-04-14 14:10:12 +0200

    new

--- Login1.php
+++ Login1.php
@@ -2,6 +2,7 @@
 
 if (isset($_POST["Username"]) && isset($_POST["Password"])) {
     include_once "credentials.php";
+
     // Create connection
     $connection = mysqli_connect($servername, $username, $password, $database);
     // Check connection
@@ -19,7 +20,19 @@ if (isset($_POST["Username"]) && isset($_POST["Password"])) {
         $row = $result->fetch_assoc();
 
         if (password_verify($_POST["Password"], $row["Password"])) {
-            print "Ok... You are now successfully registered";
+            print "Welcome " ." " . $row["First_Name"] . " " . " your last name is " . " " . $row["Second_Name"] . " " . " your age is " . " " . $row["Age"]; 
+            print "<BR>";
+           $country = $connection->prepare('SELECT * FROM countries WHERE COUNTRY_ID=?');
+           $country->bind_param('i', $row ["Nationality"]);
+           $country->execute();
+           $MyResultOfcountries = $country->get_result();
+           $counrtrySelected = $MyResultOfcountries->fetch_assoc();
+           print "you are from:". $counrtrySelected["COUNTRY_NAME"];
+           
+            
+            ?> <a href="Login1.php">Go to
+    the Login1 page</a>
+<?php
         } else {
             print "Wrong password !";
         }
