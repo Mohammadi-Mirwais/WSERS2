6174fd438373f17a0b2da0c954f0f745914b3359
Dan Durbaca <dan.durbaca@education.lu>
2020-04-29 11:10:13 +0200

    formated the sql

--- createPersons.sql
+++ createPersons.sql
@@ -1,65 +1,37 @@
 CREATE DATABASE mirwais;
 USE mirwais;
 
-CREATE TABLE user_roles
+CREATE TABLE User_roles
 (
-    ID INT NULL
-    AUTO_INCREMENT,
-usertype VARCHAR
-    (20) NOT NULL,
-PRIMARY KEY
-    (ID)
- );
-    INSERT INTO User_roles
-        (Name)
-    VALUES('Admin');
-    INSERT INTO User_roles
-        (Name)
-    VALUES('customer');
+    ID INT NULL  AUTO_INCREMENT,
+    usertype VARCHAR (20) NOT NULL,
+    PRIMARY KEY (ID)
+);
 
-    CREATE TABLE COUNTRIES
-    (
-        COUNTRY_ID INT NOT NULL
-        AUTO_INCREMENT,
-     COUNTRY_NAME VARCHAR
-        (25) NOT NULL, 
-     PRIMARY KEY
-        (COUNTRY_ID));
+INSERT INTO User_roles (Name) ('Admin');
+INSERT INTO User_roles (Name) VALUES('customer');
 
-        INSERT INTO COUNTRIES
-            (COUNTRY_NAME)
-        VALUES('Romania');
-        INSERT INTO COUNTRIES
-            (COUNTRY_NAME)
-        VALUES('USA');
-        INSERT INTO COUNTRIES
-            (COUNTRY_NAME)
-        VALUES('Luxembourg');
-        INSERT INTO COUNTRIES
-            (COUNTRY_NAME)
-        VALUES('Afghanistan');
+CREATE TABLE COUNTRIES(
+    COUNTRY_ID INT NOT NULL AUTO_INCREMENT,
+    COUNTRY_NAME VARCHAR (25) NOT NULL, 
+    PRIMARY KEY(COUNTRY_ID)
+);
 
-        CREATE TABLE people
-        (
-            PERSON_ID INT NOT NULL
-            AUTO_INCREMENT, 
-    First_Name VARCHAR
-            (25) NOT NULL, 
-    Second_Name VARCHAR
-            (25) NOT NULL,
+INSERT INTO COUNTRIES (COUNTRY_NAME) VALUES('Romania');
+INSERT INTO COUNTRIES (COUNTRY_NAME) VALUES('USA');
+INSERT INTO COUNTRIES (COUNTRY_NAME) VALUES('Luxembourg');
+INSERT INTO COUNTRIES (COUNTRY_NAME) VALUES('Afghanistan');
+
+CREATE TABLE people (
+    PERSON_ID INT NOT NULL AUTO_INCREMENT, 
+    First_Name VARCHAR (25) NOT NULL, 
+    Second_Name VARCHAR (25) NOT NULL,
     Age INT,
-    UserName VARCHAR
-            (25) NOT NULL UNIQUE,
-    Password VARCHAR
-            (250) NOT NULL,
+    UserName VARCHAR (25) NOT NULL UNIQUE,
+    Password VARCHAR (250) NOT NULL,
     Nationality INT NOT NULL,
     User_role INT NOT NULL,
-    PRIMARY KEY
-            (PERSON_ID),
-    FOREIGN KEY
-            (Nationality) REFERENCES COUNTRIES
-            (COUNTRY_ID),
-    FOREIGN KEY
-            (User_role) REFERENCES User_roles
-            (ID) 
+    PRIMARY KEY (PERSON_ID),
+    FOREIGN KEY (Nationality) REFERENCES COUNTRIES(COUNTRY_ID),
+    FOREIGN KEY (User_role) REFERENCES User_roles(ID) 
 );
